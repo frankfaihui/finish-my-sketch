f3e79ca563abf33ac33dcb8e45f02f5495d90251
Fai Hui <faifrankhui@gmail.com>
2023-04-10 15:33:37 -0700

    add GA

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,24 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import Script from 'next/script'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-X78845FTLV"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-X78845FTLV');
+        `}
+      </Script>
+      <Component {...pageProps} />
+    </>
+  )
 }
